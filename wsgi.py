a3321dc1c18e6c93213754e24512953d55958707
Ray Sulewski <rsulewski@awardsolutions.com>
2020-08-06 01:52:54 -0500

    try download results button

--- wsgi.py
+++ wsgi.py
@@ -372,6 +372,7 @@ else:
 
     @application.route('/return-file/')
     def return_file():
+    	print(session)
         if 'outputfile' in session:
             print("return_file OUTFILE = ", session['outputfile'])
             try:
